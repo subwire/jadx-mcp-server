fd857e88d30c9adb90889ec7de638ca693f63ce0
Eric Gustafson <edg@Erics-MacBook-Pro.local>
2026-05-02 18:12:25 -0700

    fix: add google() repository for smali dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,14 @@ plugins {
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
     implementation("io.github.skylot:jadx-core:1.5.5")
     implementation("io.modelcontextprotocol.sdk:mcp:0.17.1")
     implementation("org.slf4j:slf4j-simple:2.0.12")
+    runtimeOnly("io.github.skylot:jadx-dex-input:1.5.5")
 }
 
 application {
